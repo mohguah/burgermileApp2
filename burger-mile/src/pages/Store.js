4d8fb4a239752920ea276d834310e8a56e5969d0
simonhaughom <simon.haughom@gmail.com>
2022-10-24 16:51:06 +0200

    backend test

--- burger-mile/src/pages/Store.js
+++ burger-mile/src/pages/Store.js
@@ -1,11 +1,38 @@
+import ProductDisplay from '../components/ProductDisplay';
+import { useEffect, useState } from "react";
 import './Store.scss';
 
+const Message = ({ message }) => (
+    <section>
+        <p>{message}</p>
+    </section>
+);
+
 const Store = () => {
-    return (
-        <div>
-            Hello
-        </div>
-    )
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        // Check to see if this is a redirect back from Checkout
+        const query = new URLSearchParams(window.location.search);
+
+        if (query.get("success")) {
+            setMessage("Order placed! You will receive an email confirmation.");
+        }
+
+        if (query.get("canceled")) {
+            setMessage(
+                "Order canceled -- continue to shop around and checkout when you're ready."
+            );
+        }
+    }, []);
+
+    return message ? (
+        <Message message={message} />
+    ) : (
+        <ProductDisplay />
+    );
 }
+
+
 
 export default Store
